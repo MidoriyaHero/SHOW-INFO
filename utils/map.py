d375a581d17f92ecc67b35d8475ec70e9a10b17c
MidoriyaHero <buitrungtin0105@gmail.com>
2024-05-08 08:59:19 +0700

    chinh sua 1 xiu o file map.py tam thoi

--- utils/map.py
+++ utils/map.py
@@ -17,7 +17,11 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 popup_dir = os.path.join(parent_dir, "my_map.html")
 def pinpoint():
     location = streamlit_geolocation(key=1)
-    coordinate = (location['latitude'], location['longitude'])
+    if location['latitude'] and location['longitude'] is not None:
+        coordinate = (location['latitude'], location['longitude'])
+    else:
+        coordinate = (10.877600593377078, 106.80162093651423)
+   
     reverse_location = geolocator.reverse(coordinate)
 
     # Traverse the data
@@ -30,10 +34,10 @@ def pinpoint():
     city_address = address.get('county', '')
     country_address = address.get('country', '')
 
-    amenity_and_road = st.write("Address", f"{amenity_address}, {road_address}")
-    Quar = st.write("Suburb & State", f" {suburb_address}, {quarter_address}")
-    city = st.write("City", f"{city_address}")
-    country = st.write("Country", f"{country_address}")
+    st.write("Address:", f"{amenity_address}, {road_address}")
+    st.write("Suburb & State:", f" {suburb_address}, {quarter_address}")
+    st.write("City:", f"{city_address}")
+    st.write("Country:", f"{country_address}")
     m = leafmap.Map(center=coordinate, zoom=13)
     m.add_basemap(BASEMAPS)
 
